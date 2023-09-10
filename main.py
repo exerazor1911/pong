124b1baf96b060693cf7c3bff8e560aff0c0274e
exerazor1911 <exe_cien@hotmail.com>
2023-09-10 11:18:42 -0300

    Pong game

---
+++ main.py
@@ -0,0 +1,45 @@
+from turtle import Screen, Turtle
+from ball import Ball
+from paddle import Paddle
+import time
+
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor('black')
+screen.setup(800, 600)
+screen.title('Pong')
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_down, 's')
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.timer)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        ball.reset_position()
+
+    if ball.xcor() < -380:
+        scoreboard.r_point()
+        ball.reset_position()
+
+screen.exitonclick()
